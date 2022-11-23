52e00793f42c41698f6e2e70c20ddd74516da1b4
kerbless <kerbless@protonmail.com>
2022-11-23 12:15:53 +0100

    db connected with PDO

--- login.php
+++ login.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-
-<html>
-
 <?php
-	echo "hola"
-//	foreach($_POST as $key -> $value) {
-//		echo "$key: $value"
-//	}
+	try {
+		$pdo = new PDO("mysql:host=localhost;dbname=newseful;charset=UTF8", "newseful", "wWVeEeWZWYL##3EmWsZe!3M3");
+		if ($pdo) echo "connection to database was successful";
+	}
+	catch (PDOException $e) {
+		echo $e->getMessage();
+	}
 ?>
-
-</html>
